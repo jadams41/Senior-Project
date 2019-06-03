3e37fffda0600c4bdde728ef2e83a71105b3246c
jadams41 <j.ethan.adams@gmail.com>
2019-06-02 18:14:05 -0700

    commit of current dhcp progress (going to punt because documentation sucks and functionality isn't critical)

---
+++ os/src/arch/x86_64/net/ip/README.md
@@ -0,0 +1,11 @@
+## Internet Protocol Suite
+
+This directory contains files for the following IP suite protocols:
+    * IPv4 - Internet Protocol version 4
+        * _Used for creating/parsing/handling all traffic for supported IP protocols_
+    * ICMP - Internet Control Message Protocol
+        * _Used for `ping` functionality (send/receive/repsond)_
+    * UDP - Universal Datagram Protocol
+        * _Used for creating/parsing/handling UDP traffic_
+    * DHCP - Dynamic Host Configuration Protocol
+        * _Used to request IPv4 addresses from DHCP server_
---
+++ os/src/arch/x86_64/net/ip/dhcp.c
@@ -0,0 +1,57 @@
+#include <stdint-gcc.h>
+#include "net/ip/dhcp.h"
+
+
+extern rt8139_private *global_rtl_priv;
+
+/* DHCP message for requesting an ip address 
+ * NOTE: Sloppy! Can't seem to find rigid packet formats or any well-written 
+         dhcp code anywhere online?
+ * NOTE: logic taken from: https://en.wikipedia.org/wiki/Dynamic_Host_Configuration_Protocol
+ */
+/* static int create_dhcp_discover(uint8_t **packet_return){ */
+/* 	uint8_t packet[IPV4_DATA_LEN]; */
+/* 	uint32_t *long_field; */
+/* 	dhcp_packet_base *base = (dhcp_packet_base*)packet; */
+/* 	int i; */
+/* 	hw_addr hw_src = global_rtl_priv->mac_addr; */
+	
+/* 	base->opcode = 0x01; */
+/* 	base->h_type = 0x01; */
+/* 	base->h_len = 0x06; */
+/* 	base->hops = 0x00; */
+/* 	base->xid = htonl(0x3903F326); //this might be wrong */
+/* 	base->secs = 0; */
+/* 	base->flags = 0; */
+/* 	base->ciaddr = 0; */
+/* 	base->yiaddr = 0; */
+/* 	base->siaddr = 0; */
+/* 	base->giaddr = 0; */
+
+/* 	//copy over my mac address */
+/* 	for(i = 0; i < 6; i++){ */
+/* 		base->ch_addr[i] = hw_src.bytes[i]; */
+/* 	} */
+/* 	//zero out the rest of the ch_addr */
+/* 	for(i = 6; i < 16; i++){ */
+/* 		base->ch_addr[i] = 0; */
+/* 	} */
+
+/*         //zero out s_name and file fields */
+/* 	for(i = 0; i < 64; i++){ */
+/* 		base->s_name[i] = 0; */
+/* 	} */
+/* 	for(i = 0; i < 128; i++){ */
+/* 		base->file[i] = 0; */
+/* 	} */
+
+/* 	//set Magic Cookie (?) */
+/* 	long_field = (uint32_t*)(packet + sizeof(dhcp_packet_base)); */
+/* 	*long_field = htonl(0x63825363); */
+
+/* 	//set options for DHCPDISCOVER message */
+/* 	long_field += 1; */
+	
+	
+/* 	return create_ipv4_packet(IPV4_PROTO_UDP, source, dest, packet, total_packet_len, packet_return); */
+/* } */
